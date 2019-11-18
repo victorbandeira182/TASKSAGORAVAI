5294d3ca5b156bb7fc4b226665361afc12cfcb42
victorbandeira182 <victormello@id.uff.br>
2019-11-18 15:01:11 -0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	production: true,
+	firebaseConfig: {
+		apiKey: "AIzaSyAcxdhTf70urYLErI6kzuq6NuQjBVIupis",
+		authDomain: "angular-tasks-d9157.firebaseapp.com",
+		databaseURL: "https://angular-tasks-d9157.firebaseio.com",
+		projectId: "angular-tasks-d9157",
+		storageBucket: "angular-tasks-d9157.appspot.com",
+		messagingSenderId: "929615079706",
+		appId: "1:929615079706:web:548b54a7c6f3d411"
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyAcxdhTf70urYLErI6kzuq6NuQjBVIupis",
     authDomain: "angular-tasks-d9157.firebaseapp.com",
