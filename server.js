c021fc88b8316d36eba9733a5dff8e6e778f1e91
VictorBandeira <victor182_gc@hotmail.com>
2019-11-20 20:58:37 -0300

    oooo

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/TASKSAGORAVAI-master'));
+app.use(express.static(__dirname + '/dist/TODOLISTAGORAVAI'));
 
 app.get('*', function(req,res) {
-	// Replace the '/dist/<to_your_project_name>/index.html'
-	res.sendFile(path.join(__dirname+ '/dist/TASKSAGORAVAI-master/index.html'));
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname+ '/dist/TODOLISTAGORAVAI/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
